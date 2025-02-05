dcb72266eadbadc2baa9aca3afa57450ea3b38b4
Angel PS <angelpuertas@icloud.com>
2025-02-05 04:03:20 +0100

    Started tests

---
+++ test/RebaseToken.t.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {Test, console} from "forge-std/Test.sol";
+import {RebaseToken} from "src/RebaseToken.sol";
+import {Vault} from "src/Vault.sol";
+import {IRebaseToken} from "src/interfaces/IRebaseToken.sol";
+
+contract RebaseTokenTest is Test {
+    RebaseToken private rebaseToken;
+    Vault private vault;
+
+    address public owner = makeAddr("owner");
+    address public user = makeAddr("user");
+    
+    function setUp() public {
+        vm.startPrank(owner);
+        rebaseToken = new RebaseToken();
+        vault = new Vault(IRebaseToken(address(rebaseToken)));
+        rebaseToken.grantMintAndBurnRole(address(vault));
+        (bool success,) = address(payable(vault)).call{value: 1e18}("");
+        vm.stopPrank();
+    }
+
+    function test_depositLinear(uint256 amount) public {
+        amount = bound(amount, 1e5, type(uint96).max);
+        
+        vm.startPrank(user);
+        vm.deal(user, amount);
+        vault.deposit{value: amount}();
+        uint256 startBalance = rebaseToken.balanceOf(user);
+        console.log("startBalance: ", startBalance);
+
+        vm.warp(block.timestamp + 1 hours);
+        uint256 middleBalance = rebaseToken.balanceOf(user);
+        assertGt(middleBalance, startBalance);
+
+        vm.warp(block.timestamp + 1 hours);
+        uint256 endBalance = rebaseToken.balanceOf(user);
+        assertGt(endBalance, middleBalance);
+
+        assertApproxEqAbs(endBalance - middleBalance, middleBalance - startBalance, 1);
+
+        vm.stopPrank();
+    }
+}
